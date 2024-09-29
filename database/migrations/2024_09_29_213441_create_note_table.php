49f74d460bf9c545d084710de43e6a6058c559d2
Alex-OpenMimb <alexdavidhurtado@gmail.com>
2024-09-29 16:46:46 -0500

    Create database

---
+++ database/migrations/2024_09_29_213441_create_note_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('image');
+            $table->date('expiration_date');
+
+            $table->timestamps();
+        });
+
+
+        Schema::create('notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->date('creation_date');
+
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('tag_id')->constrained('tags');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+        Schema::dropIfExists('tags');
+    }
+};
