12ab7837e54a0b9a08ac838b87a4e2502b994f6a
Alex-OpenMimb <alexdavidhurtado@gmail.com>
2024-09-30 18:06:18 -0500

    Install vue and depends

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>app vue</div>
+</template>
+
+<script>
+
+export default {
+    // `setup` is a special hook dedicated for the Composition API.
+    setup() {
+
+
+        // expose the ref to the template
+
+    }
+}
+</script>
