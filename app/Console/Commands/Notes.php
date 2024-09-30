15c282e4ee95b4bfc2a49a0ef833ef26ef570869
Alex-OpenMimb <alexdavidhurtado@gmail.com>
2024-09-30 17:16:23 -0500

    Note: create by command

--- app/Console/Commands/Notes.php
+++ app/Console/Commands/Notes.php
@@ -16,7 +16,7 @@ class Notes extends Command
      * @var string
      */
     protected $signature = 'app:notes {--title=} {--description=} {--creation_date=} {--expiration_date=?} {--user=} {--tag=}';
-    //protected $signature = 'app:notes {--tag=} ';
+
 
     protected $user_id;
     protected $tag_id;
@@ -32,26 +32,38 @@ class Notes extends Command
      */
     public function handle()
     {
-        //Validate options
-         if( !self::validateOptions() ) return $this->error('Check the options passed');
-         $this->user_id = self::findUserId();
-         $this->tag_id = self::findTagId();
-         if( !$this->user_id ) return $this->error('User dosent exist');
-         if( !$this->tag_id ) return $this->error('tag dosent exist');
 
-          $creationDate = Carbon::parse($this->option('creation_date'));
-          $expirationDate = Carbon::parse($this->option('expiration_date'));
+        try {
+            //Validate options
+            if( !self::validateOptions() ) return $this->error('Check the options passed');
+            if( self::validateUniqueTitle() ) return $this->error('Error: The title already exists');
+            $this->user_id = self::findUserId();
+            $this->tag_id = self::findTagId();
+            if( !$this->user_id ) return $this->error('Error: The user dosent exist');
+            if( !$this->tag_id ) return $this->error('Error: the tag dosent exist');
 
-         Note::create([
-             'title'           => $this->option('title'),
-             'description'     => $this->option('description'),
-             'creation_date'   => $creationDate,
-             'expiration_date' => $expirationDate,
-             'user_id'         => $this->user_id->id,
-             'tag_id'          => $this->tag_id->id,
-         ]);
+            //Validate Date
+            $errorMessage = 'Error: The date is not in the correct format Y-m-d.';
+            $creationDate = self::validateFormatDate( $this->option('creation_date') );
+            if( !$creationDate ) return $this->error( $errorMessage );
+            $expirationDate = self::validateFormatDate( $this->option('expiration_date') );
+            if( !$expirationDate )  return $this->error( $errorMessage );
+            if( !$expirationDate->greaterThan( $creationDate ) ) return  $this->error('Error: The expiration date must  be later than creation date');
 
-         $this->info('Note created successfully');
+            Note::create([
+                'title'           => $this->option('title'),
+                'description'     => $this->option('description'),
+                'creation_date'   => $creationDate->format('Y-m-d'),
+                'expiration_date' => $expirationDate->format('Y-m-d'),
+                'user_id'         => $this->user_id->id,
+                'tag_id'          => $this->tag_id->id,
+            ]);
+
+            $this->info('Note created successfully');
+        }catch (\Exception $e){
+            $this->error($e->getMessage());
+        }
+
 
 
     }
@@ -93,8 +105,26 @@ class Notes extends Command
         return User::where('name', $this->option('user') )->select('id')->first();
     }
 
+
     protected function findTagId()
     {
         return Tag::where('name', $this->option('tag') )->select('id')->first();
+    }
+
+    protected function validateUniqueTitle()
+    {
+        return Note::where('title', $this->option('title'))->get()->count();
+    }
+
+
+    protected function validateFormatDate( $dateIncoming )
+    {
+         $date = Carbon::createFromFormat('Y-m-d', $dateIncoming );
+        if ($date && $date->format('Y-m-d') === $dateIncoming ) {
+            return $date;
+        } else {
+            return false;
+
+        }
     }
 }
